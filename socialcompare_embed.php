ea1bd15850ca222184b6c5d9261210c4780490db
socialcompare <socialcompare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-11 08:49:29 +0000

    update eol style. fix url regex to allow style override. add some info in the config page and readme. add comments
    git-svn-id: https://plugins.svn.wordpress.org/socialcompare-embed/trunk@371512 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- socialcompare_embed.php
+++ socialcompare_embed.php
@@ -29,8 +29,11 @@ License: GPL2
  */
 function socialcompare_shortcode($atts, $content=null, $code='') {
 	//check if the url is a SocialCompare embeddable url
-	if (preg_match('#^https?://((?:[^/]*\.)?socialcompare.com/\w\w/)(w|comparison)(/[a-z\-_0-9]+)$#i', $content, $matches)) {
+	if (preg_match('@^https?://((?:[^/]*\.)?socialcompare.com/\w\w/)(w|comparison)(/[a-z\-_0-9]+)(#.*)?$@i', $content, $matches)) {
 		$widgetUrl='http://'.$matches[1].'w'.$matches[3];
+		if (isset($matches[4])) {
+			$widgetUrl.=$matches[4];
+		}
 		//check if there is a height or width specified in the [sc] or [socialcompare] tags
 		extract(shortcode_atts(array('width' => 0, 'height' => 0), $atts));
 		if (empty($width)) { $width=socialcompare_get_width(); }
@@ -53,19 +56,22 @@ function socialcompare_shortcode($atts, $content=null, $code='') {
 function socialcompare_admin_menu(){
 	if (function_exists('add_submenu_page')) {
 		//add link under plugin for users that can manage options to allow default embed options
-		add_submenu_page('plugins.php', __('SocialCompare embed options','socialcompare'), 'SocialCompare', 'manage_options', 'socialcompare-plugin-page', 'socialcompare_plugin_page');
+		add_submenu_page('plugins.php', __('SocialCompare embed options','socialcompare-embed'), 'SocialCompare', 'manage_options', 'socialcompare-plugin-page', 'socialcompare_plugin_page');
 	}
 	add_filter('plugin_row_meta', 'socialcompare_register_plugin_links', 10, 2);
 }
 
+/** Get the default width (try socialcompare option fallback to embed size or default value) */
 function socialcompare_get_width(){
 	return socialcompare_get_size('socialcompare_width', 'embed_size_w', '100%');
 }
 
+/** Get the default height (try socialcompare option fallback to embed size or default value) */
 function socialcompare_get_height(){
 	return socialcompare_get_size('socialcompare_height', 'embed_size_h', '500');
 }
 
+/** Get the design parameter without linefeed */
 function socialcompare_get_design_stripped(){
 	$socialcompare_design=get_option('socialcompare_design','');
 	if (!empty($socialcompare_design)) {
@@ -76,6 +82,7 @@ function socialcompare_get_design_stripped(){
 }
 
 
+/** Helper function to fetch option with fallback option and value */
 function socialcompare_get_size($option_name, $option_fallback, $default){
 	$size=get_option($option_name);
 	if (empty($size)) {
@@ -87,24 +94,20 @@ function socialcompare_get_size($option_name, $option_fallback, $default){
 	return $size;
 }
 
+/** Update given option size returning a message to display or false if no modifications has been applied */
 function socialcompare_update_size($param_name){
 	if (!empty($_POST[$param_name])){
 		$size=$_POST[$param_name];
 		if (preg_match('#^\d+(%|px)?$#', $size)) {
 			update_option($param_name, $size);
-			return __('Size saved', 'socialcompare').'<br />';
+			return __('Size saved', 'socialcompare-embed').'<br />';
 		}
-		return '<strong style="color:red">'.__('Invalid size value', 'socialcompare').'</strong><br />';
+		return '<strong style="color:red">'.__('Invalid size value', 'socialcompare-embed').'</strong><br />';
 	}
 	return false;
 }
 
 function socialcompare_plugin_page(){
-	/*
-	delete_option('socialcompare_width');
-	delete_option('socialcompare_height');
-	*/
-
 	$socialcompare_width=socialcompare_get_width();
 	$socialcompare_height=socialcompare_get_height();
 	$socialcompare_design=get_option('socialcompare_design','');
@@ -127,10 +130,10 @@ function socialcompare_plugin_page(){
 					$socialcompare_design=substr($socialcompare_design, 1);
 				}
 				update_option('socialcompare_design', $socialcompare_design);
-				$result.=__('Design option saved', 'socialcompare').'<br />';
+				$result.=__('Design option saved', 'socialcompare-embed').'<br />';
 			}
 			else {
-				$result.='<strong style="color:red">'.__('Design option invalid', 'socialcompare').'</strong><br />';
+				$result.='<strong style="color:red">'.__('Design option invalid', 'socialcompare-embed').'</strong><br />';
 			}
 		}
 
@@ -138,42 +141,43 @@ function socialcompare_plugin_page(){
 			<div id="message" class="updated fade"><p><strong><?php echo $result; ?></strong></p></div>
 		<?php
 	}
-
-	//TODO hide languages option
-	//TODO hide fullscreen option
-	//TODO default css/design option
-	?><h2><?php _e('SocialCompare Configuration', 'socialcompare'); ?></h2>
-<p><?php _e('You can specify here the default size and design options to use when embedding a SocialCompare comparison table using [socialcompare] or [sc] shortcode.', 'socialcompare')?></p>
+	?><h2><?php _e('SocialCompare Configuration', 'socialcompare-embed'); ?></h2>
+<p><?php _e('You can specify here the default size and design options to use when embedding a SocialCompare comparison table using [socialcompare] or [sc] shortcode.', 'socialcompare-embed')?></p>
 <form action="" method="post">
-<p><label for="socialcompare_width"><?php _e('Default width', 'socialcompare'); ?></label> <input name="socialcompare_width" type="text" size="15" maxlength="12" value="<?php echo $socialcompare_width; ?>" /> ('100%', '500')</p>
-<p><label for="socialcompare_height"><?php _e('Default height', 'socialcompare'); ?></label> <input name="socialcompare_height" type="text" size="15" maxlength="12" value="<?php echo $socialcompare_height; ?>" />  ('450', '600')</p>
-<p><label for="socialcompare_design"><?php _e('Default design options', 'socialcompare'); ?></label> <small>(<?php _e('More details:', 'socialcompare'); ?>
+<p><label for="socialcompare_width"><?php _e('Default width', 'socialcompare-embed'); ?></label> <input name="socialcompare_width" type="text" size="15" maxlength="12" value="<?php echo $socialcompare_width; ?>" /> ('100%', '500')</p>
+<p><label for="socialcompare_height"><?php _e('Default height', 'socialcompare-embed'); ?></label> <input name="socialcompare_height" type="text" size="15" maxlength="12" value="<?php echo $socialcompare_height; ?>" />  ('450', '600')</p>
+<p><label for="socialcompare_design"><?php _e('Default design options', 'socialcompare-embed'); ?></label> <small>(<?php _e('More details:', 'socialcompare-embed'); ?>
  <a href="http://blog.socialcompare.com/2010/12/08/customize-the-embed-comparison-table/" target="_blank">[EN]</a>
  <a href="http://blog.socialcompare.com/fr/2010/12/08/personnaliser-le-tableau-comparatif-a-inclure-sur-votre-blog-ou-site-web/" target="_blank">[FR]</a>)</small>
 <br /><textarea name="socialcompare_design" cols="80" rows="3"><?php echo $socialcompare_design; ?></textarea></p>
 <p><button type="submit" name="update"><?php _e('Update options &raquo;'); ?></button></p>
 </form>
-<h2><?php _e('How to embed SocialCompare\'s comparison?', 'socialcompare'); ?></h2>
-<p><?php _e('Once you have a comparison URL you can use following syntax in your blog post or page.', 'socialcompare')?></p>
+<h2><?php _e('How to embed SocialCompare\'s comparison?', 'socialcompare-embed'); ?></h2>
+<p><?php _e('Once you have a comparison URL you can use following syntax in your blog post or page.', 'socialcompare-embed')?></p>
 <code>[socialcompare]http://socialcompare.com/en/w/apples-and-oranges[/socialcompare]</code><br />
 <br />
 <code>[sc]http://socialcompare.com/en/comparison/apples-and-oranges[/sc]</code>
-<p><?php _e('You can override the default dimensions specified above using this syntax.', 'socialcompare')?></p>
+<p><?php _e('You can override the default dimensions specified above using this syntax.', 'socialcompare-embed')?></p>
 <code>[socialcompare width="50%" height="300"]http://socialcompare.com/en/comparison/apples-and-oranges[/socialcompare]</code><br />
 <br />
 <code>[sc height="600" width="500"]http://socialcompare.com/en/comparison/apples-and-oranges[/sc]</code>
+<p><?php _e('You can override the default design specified above using this syntax.', 'socialcompare-embed')?></p>
+<code>[socialcompare width="50%" height="300"]http://socialcompare.com/en/comparison/apples-and-oranges#nofull;noflags[/socialcompare]</code><br />
+<br />
+<code>[sc]http://socialcompare.com/en/comparison/apples-and-oranges#noedit[/sc]</code>
 	<?php
 }
 
+
+/** Helper to get the relative URL to our plugin directory */
 function socialcompare_get_basename(){
 	return plugin_basename(__FILE__);
 }
 
 function socialcompare_register_plugin_links($links, $file) {
 	$base=socialcompare_get_basename();
-	if ($file==$base){
+	if ($file==$base){ // filter is executed on all entries so check it's our own plugin and add a link to the settings
 		$links[]='<a href="plugins.php?page=socialcompare-plugin-page">'.__('Settings').'</a>';
-		//XXX already exists $links[]='<a href="http://socialcompare.com/">SocialCompare.com</a>';
 	}
 	return $links;
 }
@@ -194,3 +198,5 @@ function socialcompare_init(){
 if(defined('ABSPATH') && defined('WPINC')) {
 	add_action('init','socialcompare_init');
 }
+
+//no closing php to avoid blank space insertion
